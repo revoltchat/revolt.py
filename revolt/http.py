e2f68f924bb28016468fbe128383e0b3a38fe79b
rockboy987 <62481809+rockboy987@users.noreply.github.com>
2021-09-03 22:42:42 +0530

    Fix missing comma

--- revolt/http.py
+++ revolt/http.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import (TYPE_CHECKING, Any, Coroutine, Literal, Optional, overload TypeVar,
+from typing import (TYPE_CHECKING, Any, Coroutine, Literal, Optional, overload, TypeVar,
                     Union)
 
 import aiohttp
