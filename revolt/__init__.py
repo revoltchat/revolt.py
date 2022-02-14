40e9e3a26e35f9388d7fdcae5be9c67cccf9539a
Zomatree <angelokontaxis@hotmail.com>
2022-02-14 05:50:50 +0000

    import *

--- revolt/__init__.py
+++ revolt/__init__.py
@@ -1,21 +1,19 @@
 from . import utils
-from .asset import Asset
-from .category import Category
-from .channel import (Channel, DMChannel, GroupDMChannel, SavedMessageChannel,
-                      TextChannel, VoiceChannel)
-from .client import Client
-from .embed import Embed
-from .enums import (AssetType, ChannelType, PresenceType, RelationshipType,
-                    SortType)
-from .errors import HTTPError, RevoltError, ServerError
-from .file import File
-from .flags import UserBadges
-from .member import Member
-from .message import Masquerade, Message, MessageReply
-from .messageable import Messageable
-from .permissions import ChannelPermissions, ServerPermissions
-from .role import Role
-from .server import Server, SystemMessages
-from .user import Relation, Status, User
+from .asset import *
+from .category import *
+from .channel import *
+from .client import *
+from .embed import *
+from .enums import *
+from .errors import *
+from .file import *
+from .flags import *
+from .member import *
+from .message import *
+from .messageable import *
+from .permissions import *
+from .role import *
+from .server import *
+from .user import *
 
 __version__ = (0, 1, 6)
