7ab78c7ceabc5b1ced8a3b5392375e306ba24e37
Omkaar <79257339+Pysics@users.noreply.github.com>
2022-03-26 11:01:31 +0530

    Fix error in `utils.py`

--- revolt/utils.py
+++ revolt/utils.py
@@ -25,7 +25,7 @@ def copy_doc(from_t: T) -> Callable[[T], T]:
 R_T = TypeVar("R_T")
 P = ParamSpec("P")
 
-# it is impossible to type this function correctly for a couple reasons:
+# it is impossible to type this function correctly for a couple of reasons:
 # 1. isawaitable does not narrow while keeping typevars - there is an open PR for this (typeshed#5658) but it cannot be merged because mypy does not support the feature fully
 # 2. typeguard does not narrow for the negative case which is dumb in my opinion, so `value` would stay being a union even after the if statement (PEP 647 - "The type is not narrowed in the negative case")
 
